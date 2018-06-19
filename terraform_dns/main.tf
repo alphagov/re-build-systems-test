4dae56de1b65913df1e3d0dbf7b6275f65bcbaeb
David Povey <david.povey@digital.cabinet-office.gov.uk>
2018-06-19 15:56:09 +0100

    Add helpful instructions for users
    Solo @poveyd

--- terraform_dns/main.tf
+++ terraform_dns/main.tf
@@ -42,3 +42,13 @@ resource "aws_eip" "jenkins2_eips" {
     Team      = "${var.team_name}"
   }
 }
+
+resource "null_resource" "Instructions" {
+  depends_on = ["aws_route53_zone.primary_zone"]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "\n*****************************\n\nPlease send the following information to whichever team in your organisation looks after the domain name ${var.top_level_domain_name}: \n${var.team_name} = ['${join(".','", aws_route53_zone.primary_zone.name_servers)}.']\n\n*****************************"
+    EOT
+  }
+}
