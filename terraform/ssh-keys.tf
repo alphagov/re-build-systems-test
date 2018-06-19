1ef21437177406aaf130176a26f4e7b7f9dd3df2
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-06-19 18:43:13 +0100

    Fix variable use for ssh_public_key_file
    Adding missing the "var." prefix
    
    Solo: @smford

--- terraform/ssh-keys.tf
+++ terraform/ssh-keys.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "deployer-ssh-key" {
   key_name   = "jenkins2_key_${var.team_name}_${var.environment}"
-  public_key = "${file("${ssh_public_key_file}")}"
+  public_key = "${file("${var.ssh_public_key_file}")}"
 }
