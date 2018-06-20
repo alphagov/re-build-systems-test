ce6ea4911c0fa7d9d0a1bb23742d26af4276b2f2
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-06-20 01:15:11 +0100

    Adding creation of TLS Certificates
    Solo: @smford

---
+++ terraform/tls.tf
@@ -0,0 +1,20 @@
+variable "san_domains" {
+  default = [
+    "${var.server_name}.${var.environment}.${var.team_name}.${var.hostname_suffix}",
+  ]
+}
+
+resource "aws_acm_certificate" "tls_certificate" {
+  domain_name               = "${var.environment}.${var.team_name}.${var.hostname_suffix}"
+  validation_method         = "DNS"
+  subject_alternative_names = "${var.san_domains}"
+}
+
+resource "aws_route53_record" "dns_validation" {
+  count   = "${length(var.san_domains) + 1}"
+  zone_id = "${data.terraform_remote_state.team_dns_and_eips.team_zone_id}"
+  name    = "${lookup(aws_acm_certificate.tls_certificate.domain_validation_options[count.index], "resource_record_name")}"
+  type    = "${lookup(aws_acm_certificate.tls_certificate.domain_validation_options[count.index], "resource_record_type")}"
+  records = ["${lookup(aws_acm_certificate.tls_certificate.domain_validation_options[count.index], "resource_record_value")}"]
+  ttl     = 60
+}
