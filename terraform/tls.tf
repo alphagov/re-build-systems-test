699e6373bb6f93bbeece5da35167050a12c14187
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-06-20 15:18:01 +0100

    Change variable to be local to allow interpolation
    Variables do not allow interpolation, changed to type local.
    
    Solo: @smford

--- terraform/tls.tf
+++ terraform/tls.tf
@@ -1,17 +1,17 @@
-variable "san_domains" {
-  default = [
-    "${var.server_name}.${var.environment}.${var.team_name}.${var.hostname_suffix}",
+locals {
+  san_domains = [
+    "www.${var.server_name}.${var.environment}.${var.team_name}.${var.hostname_suffix}",
   ]
 }
 
 resource "aws_acm_certificate" "tls_certificate" {
-  domain_name               = "${var.environment}.${var.team_name}.${var.hostname_suffix}"
+  domain_name               = "${var.server_name}.${var.environment}.${var.team_name}.${var.hostname_suffix}"
   validation_method         = "DNS"
-  subject_alternative_names = "${var.san_domains}"
+  subject_alternative_names = "${local.san_domains}"
 }
 
 resource "aws_route53_record" "dns_validation" {
-  count   = "${length(var.san_domains) + 1}"
+  count   = "${length(local.san_domains) + 1}"
   zone_id = "${data.terraform_remote_state.team_dns_and_eips.team_zone_id}"
   name    = "${lookup(aws_acm_certificate.tls_certificate.domain_validation_options[count.index], "resource_record_name")}"
   type    = "${lookup(aws_acm_certificate.tls_certificate.domain_validation_options[count.index], "resource_record_type")}"
