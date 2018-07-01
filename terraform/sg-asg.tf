712fc5852010e588885bdc426d66aa78f41e09eb
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-07-01 13:31:46 +0100

    Update security groups
    - updated security group naming where it applies to the asg
    - removed ssh access to the SG which gets applied to the ELB as ssh is not possible via elb
    
    Solo: @smford

--- terraform/sg-asg.tf
+++ terraform/sg-asg.tf
@@ -1,8 +1,8 @@
-module "jenkins2_sg_asg_server" {
+module "jenkins2_sg_asg_server_internal" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "1.22.0"
 
-  name        = "jenkins2_sg_asg_server_${var.team_name}_${var.environment}"
+  name        = "jenkins2_sg_asg_server_internal_${var.team_name}_${var.environment}"
   description = "Jenkins2 Security Group Allowing Server to Worker Communication"
   vpc_id      = "${module.jenkins2_vpc.vpc_id}"
 
@@ -22,4 +22,17 @@ module "jenkins2_sg_asg_server" {
       cidr_blocks = "${var.public_subnet}"
     },
   ]
+}
+
+module "jenkins2_sg_asg_server_internet_facing" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "1.22.0"
+
+  name        = "jenkins2_sg_asg_server_internet_facing_${var.team_name}_${var.environment}"
+  description = "Jenkins2 Security Group Allowing HTTP and SSH from Internet"
+  vpc_id      = "${module.jenkins2_vpc.vpc_id}"
+
+  ingress_cidr_blocks = ["${var.allowed_ips}"]
+  ingress_rules       = ["https-443-tcp", "all-icmp", "ssh-tcp"]
+  egress_rules        = ["all-all"]
 }
