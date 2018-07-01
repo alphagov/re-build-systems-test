7f467c4bd3152a8feb2bcc6a58b0883be3c41e79
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-07-01 13:53:23 +0100

    Move EFS security group to own file to match standard
    As per our standard the security group for EFS has been moved to its own file.
    
    Solo: @smford

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -14,22 +14,3 @@ resource "aws_efs_mount_target" "jenkins2_efs_master_mount" {
   subnet_id       = "${element(module.jenkins2_vpc.public_subnets,0)}"
   security_groups = ["${module.jenkins2_sg_efs.this_security_group_id}"]
 }
-
-module "jenkins2_sg_efs" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "1.22.0"
-
-  name        = "jenkins2_sg_efs_${var.team_name}_${var.environment}"
-  description = "Jenkins2 Security Group Allowing EFS Access"
-  vpc_id      = "${module.jenkins2_vpc.vpc_id}"
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 2049
-      to_port     = 2049
-      protocol    = "tcp"
-      description = "Allow connection from the servers to EFS"
-      cidr_blocks = "${var.public_subnet}"
-    },
-  ]
-}
