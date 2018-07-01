f5f7eacd8cbbe9a6fb5d022afbd2e50830408f1b
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-07-01 15:54:19 +0100

    Tidy up terraform outputs
    - re-order outputs to be alphabetical
    - added jenkins_url
    - removed deprecated outputs and commented out junk
    
    Solo: @smford

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,27 +1,34 @@
+output "dns_state_bucket" {
+  value = "tfstate-dns-${var.team_name}.${var.hostname_suffix}"
+}
+
+output "dns_state_file" {
+  value = "${var.team_name}.${var.hostname_suffix}.tfstate"
+}
+
+output "environment" {
+  value = "${var.environment}"
+}
+
 output "image_id" {
   value = "${data.aws_ami.source.id}"
 }
 
-# Commented because this DNS name resolves to the original public IPv4 address of the EC2. We need the public DNS name that resolves to the eip.
-# output "jenkins2_dns_name" {
-#   description = "Jenkins2 DNS name - uri of the EC2 instance created"
-#   value = ["${module.jenkins2_server.public_dns}"]
-# }
+output "jenkins2_env_eip_ids" {
+  value = "${data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ids}"
+}
 
-#output "jenkins2_sg_server_internet_facing_id" {
-#  description = "jenkins2 server internet security group id"
-#  value       = "${module.jenkins2_sg_server_internet_facing.this_security_group_id}"
-#}
+output "jenkins2_env_eip" {
+  value = "${lookup(data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ips, var.environment)}"
+}
 
-#output "jenkins2_sg_worker_id" {
-#  description = "jenkins2 worker security group id"
-#  value       = "${module.jenkins2_sg_worker.this_security_group_id}"
-#}
+output "jenkins2_env_eip_ips" {
+  value = "${data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ips}"
+}
 
-#output "jenksin2_server_public_ip" {
-#  description = "jenkins2 server public ip"
-#  value       = ["${module.jenkins2_server.public_ip}"]
-#}
+output "jenkins2_url" {
+  value = "https://${var.server_name}.${var.environment}.${var.team_name}.${var.hostname_suffix}"
+}
 
 output "jenkins2_vpc_id" {
   description = "The ID of the VPC"
@@ -38,30 +45,10 @@ output "public_subnets" {
   value       = ["${module.jenkins2_vpc.public_subnets}"]
 }
 
-output "jenkins2_env_eip_ids" {
-  value = "${data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ids}"
-}
-
-output "jenkins2_env_eip_ips" {
-  value = "${data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ips}"
-}
-
-output "jenkins2_env_eip" {
-  value = "${lookup(data.terraform_remote_state.team_dns_and_eips.jenkins2_env_eip_ips, var.environment)}"
-}
-
 output "team_domain_name" {
   value = "${data.terraform_remote_state.team_dns_and_eips.team_domain_name}"
 }
 
 output "team_zone_id" {
   value = "${data.terraform_remote_state.team_dns_and_eips.team_zone_id}"
-}
-
-output "dns_state_bucket" {
-  value = "tfstate-dns-${var.team_name}.${var.hostname_suffix}"
-}
-
-output "dns_state_file" {
-  value = "${var.team_name}.${var.hostname_suffix}.tfstate"
 }
