35157f91dcb1fa62127e4a1aa74a3713b7c12c63
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-07-01 14:21:58 +0100

    Allow AZ for an ELB to be specified.
    - change the name of the ELB (it doesn't like dots in its name)
    - use the user defined AZ when provisioning the ELB
    
    Solo: @smford

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -83,10 +83,10 @@ resource "aws_autoscaling_group" "asg_jenkins2_server" {
 }
 
 resource "aws_elb" "elb_jenkins2_server" {
-  name = "elb-${var.server_name}.${var.environment}.${var.team_name}"
+  name = "elb-${var.server_name}-${var.environment}-${var.team_name}"
 
-  # availability_zones = ["eu-west-2a"]
-  security_groups = ["${module.jenkins2_sg_asg_server_internet_facing.this_security_group_id}", "${module.jenkins2_sg_asg_server_internal.this_security_group_id}"]
+  availability_zones = ["${var.aws_az}"]
+  security_groups    = ["${module.jenkins2_sg_asg_server_internet_facing.this_security_group_id}", "${module.jenkins2_sg_asg_server_internal.this_security_group_id}"]
 
   subnets = ["${element(module.jenkins2_vpc.public_subnets,0)}"]
 
