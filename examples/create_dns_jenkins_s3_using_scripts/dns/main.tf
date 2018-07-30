702967b2fe3ed086101a54312023d53dea725dc4
David Povey <david.povey@digital.cabinet-office.gov.uk>
2018-07-30 14:23:47 +0100

    Add reference to link to module.
    Solo: @poveyd

--- examples/create_dns_jenkins_s3_using_scripts/dns/main.tf
+++ examples/create_dns_jenkins_s3_using_scripts/dns/main.tf
@@ -1,7 +1,6 @@
-# Example file for a team.
-
 module "terraform_dns" {
-  source  = "../../../dns"
+  # The next line needs to be a link to where the DNS module has been downloaded.
+  source  = "./dns_module"
   version = "1.0.0"
 
   team_name = "${var.team_name}"
