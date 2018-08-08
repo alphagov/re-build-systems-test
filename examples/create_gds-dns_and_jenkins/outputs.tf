77c348efa1580f69b8731bba55b5a30b2020001c
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-08-08 13:10:47 +0100

    Complete example of full DNS and Jenkins Module Use
    This example deploys a teams dns and jenkins instance within a single terraform apply.
    
    If used by an team wishing to use the build.gds-reliability.engineering domain it is neccessary for the RE Build team to delegate DNS resolution to allow the ACM certificate to be DNS validated and issued.
    
    Solo: @smford

---
+++ examples/create_gds-dns_and_jenkins/outputs.tf
@@ -0,0 +1,38 @@
+output "environment" {
+  value = "${var.environment}"
+}
+
+output "github_callback_url" {
+  value = "${module.jenkins.github_callback_url}"
+}
+
+output "image_id" {
+  value = "${module.jenkins.image_id}"
+}
+
+output "jenkins2_url" {
+  value = "${module.jenkins.jenkins2_url}"
+}
+
+output "jenkins2_vpc_id" {
+  description = "The ID of the VPC"
+  value       = "${module.jenkins.jenkins2_vpc_id}"
+}
+
+output "jenkins2_worker_public_ip" {
+  description = "jenkins2 worker public ip address"
+  value       = ["${module.jenkins.jenkins2_worker_public_ip}"]
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = ["${module.jenkins.public_subnets}"]
+}
+
+# output "team_domain_name" {
+#   value = "${module.jenkins.team_domain_name}"
+# }
+
+output "team_zone_id" {
+  value = "${module.dns.team_zone_id}"
+}
---
+++ examples/create_gds-dns_and_jenkins/variables.tf
@@ -0,0 +1,139 @@
+variable "allowed_ips" {
+  description = "A list of IP addresses permitted to access (via SSH & HTTPS) the EC2 instances created that are running Jenkins"
+  type        = "list"
+}
+
+variable "aws_az" {
+  description = "Single availability zone to place master and worker instances in (a,b,ci)"
+  type        = "string"
+  default     = "a"
+}
+
+variable "aws_profile" {
+  description = "AWS profile"
+  type        = "string"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = "string"
+}
+
+variable "docker_version" {
+  description = "The version of docker to install"
+  type        = "string"
+}
+
+variable "environment" {
+  description = "Environment name (e.g. production, test, ci). This is used to construct the DNS name for your Jenkins instances"
+  type        = "string"
+}
+
+variable "github_admin_users" {
+  description = "List of Github admin users"
+  type        = "list"
+  default     = []
+}
+
+variable "github_client_id" {
+  description = "Your Github Auth client ID"
+  type        = "string"
+  default     = ""
+}
+
+variable "github_client_secret" {
+  description = "Your Github Auth client secret"
+  type        = "string"
+  default     = ""
+}
+
+variable "github_organisations" {
+  description = "List of Github organisations and teams that users must be a member of to allow HTTPS login to master"
+  type        = "list"
+  default     = []
+}
+
+variable "gitrepo" {
+  description = "Git repo that hosts Dockerfile"
+  type        = "string"
+}
+
+variable "gitrepo_branch" {
+  description = "Branch of git repo that hosts Dockerfile"
+  type        = "string"
+  default     = "master"
+}
+
+variable "hostname_suffix" {
+  description = "Main domain name for new Jenkins instances, e.g. example.com"
+  type        = "string"
+}
+
+variable "jenkins_version" {
+  description = "Verson of jenkins to install"
+  type        = "string"
+  default     = "latest"
+}
+
+variable "server_instance_type" {
+  description = "This defines the default master server EC2 instance type"
+  type        = "string"
+  default     = "t2.small"
+}
+
+variable "server_name" {
+  description = "Hostname of the jenkins2 master"
+  type        = "string"
+  default     = "jenkins2"
+}
+
+variable "server_root_volume_size" {
+  description = "Size of the Jenkins Server root volume (GB)"
+  type        = "string"
+  default     = "50"
+}
+
+variable "server_user_data" {
+  description = "User Data that gets inserted into cloud-init template"
+  type        = "string"
+  default     = ""
+}
+
+variable "ssh_public_key_file" {
+  description = "Location of public key used to access the server instances"
+  type        = "string"
+}
+
+variable "team_name" {
+  description = "Name of your team. This is used to construct the DNS name for your Jenkins instances"
+  type        = "string"
+}
+
+variable "ubuntu_release" {
+  description = "Which version of ubuntu to install"
+  type        = "string"
+}
+
+variable "worker_instance_type" {
+  description = "This defines the default (AWS) instance type"
+  type        = "string"
+  default     = "t2.medium"
+}
+
+variable "worker_name" {
+  description = "Name of the jenkins2 worker"
+  type        = "string"
+  default     = "worker"
+}
+
+variable "worker_root_volume_size" {
+  description = "Size of the Jenkins Worker root volume (GB)"
+  type        = "string"
+  default     = "50"
+}
+
+variable "worker_user_data" {
+  description = "User Data that gets inserted into cloud-init template"
+  type        = "string"
+  default     = ""
+}
