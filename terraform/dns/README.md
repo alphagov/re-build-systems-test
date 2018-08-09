0774463dc40ec71ab1dd6f1b2ae671263c88dbe3
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-08-09 08:58:03 +0100

    Remove code which is duplicated in the new terraform modules
    Solo: @smford

--- terraform/dns/README.md
+++
@@ -1,3 +0,0 @@
-# Reliability Engineering - Build Systems - Team DNS Configuration
-
-Refer to the main README.md file of the repository.
--- terraform/jenkins/dns-remote-state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "team_dns" {
-  backend = "s3"
-
-  config {
-    bucket = "tfstate-dns-${var.team_name}.${var.hostname_suffix}"
-    key    = "${var.team_name}.${var.hostname_suffix}.tfstate"
-    region = "${var.aws_region}"
-  }
-}
