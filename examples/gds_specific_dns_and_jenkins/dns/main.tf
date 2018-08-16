fa2dfde56d0d9eb03bf8b8ee6b13aa64a0c8e98a
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-08-16 18:41:14 +0100

    Remove module version pinning from examples
    The examples had the module versions pinned to v0.0.1, this causes maintenance overhead and confusion by the end user.  Instead I have removed module version pinning (to releases) meaning they use the latest (master branch) and have added inline documentation detailing how one may choose to pin if needed.
    
    Solo: @smford

--- examples/gds_specific_dns_and_jenkins/dns/main.tf
+++ examples/gds_specific_dns_and_jenkins/dns/main.tf
@@ -1,5 +1,8 @@
 module "dns" {
-  source = "git::https://github.com/alphagov/terraform-aws-re-build-dns.git?ref=0.0.1"
+  source = "git::https://github.com/alphagov/terraform-aws-re-build-dns.git"
+
+  # If a specific release is needed rather than "latest", the below syntax can be used.
+  # source = "git::https://github.com/alphagov/terraform-aws-re-build-dns.git?ref=0.0.1"
 
   team_name       = "${var.team_name}"
   hostname_suffix = "${var.hostname_suffix}"
