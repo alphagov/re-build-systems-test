d9ee1764043e5a980e04fecf152f65055dc32c87
David Povey <david.povey@digital.cabinet-office.gov.uk>
2018-08-21 19:17:40 +0100

    Files which can be used to add custom cloud-init commands.
    Solo: @poveyd

---
+++ examples/gds_specific_dns_and_jenkins/jenkins/cloud-init/server_specific_cloud_init.sh
@@ -0,0 +1,2 @@
+echo "Start of custom cloud-init configuration on the server"
+echo "End of custom cloud-init configuration on the server"
---
+++ examples/gds_specific_dns_and_jenkins/jenkins/cloud-init/worker_specific_cloud_init.sh
@@ -0,0 +1,2 @@
+echo "Start of custom cloud-init configuration on the worker"
+echo "End of custom cloud-init configuration on the worker"
