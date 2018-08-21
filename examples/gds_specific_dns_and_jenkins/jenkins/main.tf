97e663c5ea528a178bb6c7d605e54c213bfa903e
David Povey <david.povey@digital.cabinet-office.gov.uk>
2018-08-21 19:22:52 +0100

    Changes to append custom cloud-init commands to default cloud-init script.
    Solo: @poveyd

--- examples/gds_specific_dns_and_jenkins/jenkins/main.tf
+++ examples/gds_specific_dns_and_jenkins/jenkins/main.tf
@@ -1,5 +1,5 @@
 module "jenkins" {
-  source = "git::https://github.com/alphagov/terraform-aws-re-build-jenkins.git"
+  source = "../../../../terraform-aws-re-build-jenkins/"
 
   # If a specific release is needed rather than "latest", the below syntax can be used.
   # source = "git::https://github.com/alphagov/terraform-aws-re-build-jenkins.git?ref=0.0.2"
@@ -10,31 +10,27 @@ module "jenkins" {
   environment = "${var.environment}"
   region      = "${var.aws_region}"
   team_name   = "${var.team_name}"
-
   # Git repo
   gitrepo        = "${var.gitrepo}"
   gitrepo_branch = "${var.gitrepo_branch}"
-
   # Github auth configuration
   github_admin_users   = ["${join(",", var.github_admin_users)}"]
   github_client_id     = "${var.github_client_id}"
   github_client_secret = "${var.github_client_secret}"
   github_organisations = ["${join(",", var.github_organisations)}"]
-
   # Public key
   ssh_public_key_file = "${var.ssh_public_key_file}"
-
   # DNS configuration
   hostname_suffix      = "${var.hostname_suffix}"
   route53_team_zone_id = "${data.terraform_remote_state.team_dns.team_zone_id}"
-
   # Server Configuration
   server_instance_type    = "${var.server_instance_type}"
   server_name             = "${var.server_name}"
   server_root_volume_size = "${var.server_root_volume_size}"
-
+  append_server_user_data = "${var.append_server_user_data}"
   # Worker Configuration
   worker_instance_type    = "${var.worker_instance_type}"
   worker_name             = "${var.worker_name}"
   worker_root_volume_size = "${var.worker_root_volume_size}"
+  append_worker_user_data = "${var.append_worker_user_data}"
 }
