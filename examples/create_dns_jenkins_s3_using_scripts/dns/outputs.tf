e9a196e73baaca1916a6bb6513c882aa667be055
daniele-occhipinti <contact@danieleocchipinti.com>
2018-09-06 11:48:53 +0100

    Remove some examples, as they still need to be review
    Some of the examples haven't been checked against the tech writer guidelines
    and not yet been fully tested after the new way of modularising our Terraform code.
    
    We are going to remove them for now until we have time to go through them.
    
    I have checked this with Dave Povey, and he is OK about that.
    
    A card in the backlog has been created:
    https://trello.com/c/hqzhmgsp/225-review-other-examples-that-we-have-temporarily-removed

--- examples/create_dns_jenkins_s3_using_scripts/dns/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "team_zone_id" {
-  value = "${module.terraform_dns.team_zone_id}"
-}
-
-output "team_zone_nameservers" {
-  value = "${module.terraform_dns.team_zone_nameservers}"
-}
--- examples/dns_with_embedded_s3/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bucket_name" {
-  value = "${aws_s3_bucket.dns_bucket.bucket_domain_name}"
-}
